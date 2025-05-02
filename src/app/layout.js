d58ac7841d6f08b1e8baaf7f1a29dca348f9aac8
Bridget Amana <amanabridget03@gmail.com>
2025-05-02 17:01:14 +0100

    update layout structure by creating ClientLayout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,11 @@
-"use client"
-import Header from "@/components/Header";
-import { Playfair } from "next/font/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/Footer";
-import ScrollArrow from "@/components/ScrollArrow";
-import { useEffect } from "react";
-import { usePathname } from "next/navigation";
+import ClientLayout from "./clientLayout";
 
-const playfair = Playfair({
-  subsets: ["latin"],
-  variable: "--font-playfair",
-  display: "swap",
-  weight: ["400", "500", "600", "700"],
-  style: ["normal", "italic"],
-  preload: true,
-  fallback: ["system-ui", "sans-serif"],
-});
+export const metadata = {
+  title: "Bridget Amana",
+  description: "portfolio",
+};
 
 const inter = Inter({
   subsets: ["latin"],
@@ -29,21 +18,9 @@ const inter = Inter({
 });
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
   return (
-    <html lang="en" className={inter.className}>
-      <body className="min-h-screen flex flex-col bg-background text-foreground">
-        <Header />
-        <main className="flex-1">
-          {children}
-        </main>
-        <Footer />
-        <ScrollArrow position={pathname === '/' ? 'center' : 'bottom-left'} />
-      </body>
+    <html lang="en" className={inter.className} suppressHydrationWarning>
+      <ClientLayout>{children}</ClientLayout>
     </html>
   );
 }
